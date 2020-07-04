eca0d1727acefde6feac63281198b9d1cee985a1
surendhar <surendhar.raj2@gmail.com>
2020-07-04 20:01:53 +0530

    Revert "fixed setting not imported issue"
    This reverts commit 6a16cd480a371a152815f4251ad99d23b72796c1.

--- UserActivity/settings.py
+++ UserActivity/settings.py
@@ -53,7 +53,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'UserActivity.UserActivity.user.authentication.TokenAuthentication', # to allow token based auth
+        'UserActivity.user.authentication.TokenAuthentication', # to allow token based auth
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
